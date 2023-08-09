ada0371370f2d4600c9ba6f8a3b3f22ad18014e0
Yvesei <musheck999@gmail.com>
2023-08-09 19:06:17 +0100

    Dashboard page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import SignUp from './components/SignUp.vue'
-import Login from './components/Login.vue'
-import Navbar from './components/Navbar.vue'
-import Canva from './components/Canva.vue'
-import Hero from './components/Hero.vue'
-import Footer from './components/Footer.vue'
-import Post from './components/Post.vue'
+import SignUp from "./components/SignUp.vue";
+import Login from "./components/Login.vue";
+import Navbar from "./components/Navbar.vue";
+import Canva from "./components/Canva.vue";
+import Hero from "./components/Hero.vue";
+import Footer from "./components/Footer.vue";
+import Post from "./components/Post.vue";
 </script>
 
 <template>
@@ -15,11 +15,9 @@ import Post from './components/Post.vue'
 </template>
 
 <script>
-
-
 export default {
-  name:'app',
-  components:{
+  name: "app",
+  components: {
     SignUp,
     Login,
     Navbar,
@@ -27,7 +25,6 @@ export default {
     Hero,
     Footer,
     Post,
-  }
-}
+  },
+};
 </script>
-
