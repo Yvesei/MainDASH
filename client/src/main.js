b1ca67abc121deee16c650ad49d17b77c3990b2d
Yvesei <musheck999@gmail.com>
2023-08-16 18:29:40 +0100

    slight edit

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,24 @@
-import './style.css'
+import "./style.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import './axios'
-
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./axios";
 
 /* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 
-import router from './router'
+import router from "./router";
 
 /* add icons to the library */
-library.add(faUserSecret)
+library.add(faUserSecret);
 
 createApp(App)
-.component('font-awesome-icon', FontAwesomeIcon).use(router)
-.mount('#app')
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
