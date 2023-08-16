86226f5cea5e20e400e37a5e50a330b6c324f601
Yvesei <musheck999@gmail.com>
2023-08-16 18:30:09 +0100

    Create user + Modify user popup Added

--- client/src/components/CreateUser.vue
+++ client/src/components/CreateUser.vue
@@ -3,7 +3,7 @@
     v-if="showUserPopup"
     class="h-full overflow-auto w-full fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center"
   >
-    <form class="relative w-2/3" style="height: 406px">
+    <form class="relative w-2/3 h-[80%]">
       <div
         active=""
         form="user"
@@ -11,11 +11,49 @@
       >
         <div class="flex justify-between">
           <h5 class="mb-0 font-bold dark:text-white">Client</h5>
-          <h5 @click="closeUserPopup">X</h5>
+
+          <svg
+            @click="closeUserPopup"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            class="h-6"
+          >
+            <path
+              d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c-9.4 9.4-9.4 24.6 0 33.9l47 47-47 47c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l47-47 47 47c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-47-47 47-47c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-47 47-47-47c-9.4-9.4-24.6-9.4-33.9 0z"
+            />
+          </svg>
         </div>
         <div>
+          <!-- component -->
+
+          <div
+            class="flex flex-row items-center justify-center bg-grey-lighter round"
+            style="flex-direction: column"
+          >
+            <label
+              class="flex flex-col items-center px-6 py-6 mb-4 mt-8 bg-white text-blue rounded-full shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white"
+            >
+              <svg
+                class="w-8 h-8"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
+                />
+              </svg>
+
+              <input type="file" class="hidden" />
+            </label>
+            <label
+              class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
+              for="First Name"
+              >Profile</label
+            >
+          </div>
           <div class="flex flex-wrap mt-4 -mx-3">
-            <div class="w-full max-w-full px-3 flex-0 sm:w-6/12">
+            <div class="w-full max-w-full px-3 flex-0">
               <label
                 class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
                 for="First Name"
@@ -28,7 +66,9 @@
                 class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
               />
             </div>
-            <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0 sm:w-6/12">
+          </div>
+          <div class="flex flex-wrap mt-4 -mx-3">
+            <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0">
               <label
                 class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
                 for="Last Name"
@@ -43,7 +83,7 @@
             </div>
           </div>
           <div class="flex flex-wrap mt-4 -mx-3">
-            <div class="w-full max-w-full px-3 flex-0 sm:w-6/12">
+            <div class="w-full max-w-full px-3 flex-0">
               <label
                 class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
                 for="First Name"
@@ -56,28 +96,42 @@
                 class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
               />
             </div>
-            <div class="w-full max-w-full px-3 flex-0 sm:w-6/12">
+          </div>
+          <div class="flex flex-wrap mt-4 -mx-3">
+            <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0">
               <label
                 class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
-                for="First Name"
-                >Image</label
+                for="Last Name"
+                >Password</label
               >
               <input
-                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                id="file_input"
-                type="file"
+                type="password"
+                name="Last Name"
+                placeholder="eg. Prior"
+                class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
               />
             </div>
           </div>
           <div class="flex mt-6">
             <button
+              v-if="create"
               type="button"
               aria-controls="address"
               next-form-btn=""
               href="javascript:;"
               class="inline-block px-6 py-3 mb-0 ml-auto text-xs font-bold text-right text-white uppercase align-middle transition-all ease-in border-0 rounded-lg shadow-md cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 bg-gradient-to-tl from-zinc-800 to-zinc-700 leading-pro tracking-tight-rem bg-150 bg-x-25"
             >
-              Next
+              Create
+            </button>
+            <button
+              v-if="!create"
+              type="button"
+              aria-controls="address"
+              next-form-btn=""
+              href="javascript:;"
+              class="inline-block px-6 py-3 mb-0 ml-auto text-xs font-bold text-right text-white uppercase align-middle transition-all ease-in border-0 rounded-lg shadow-md cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 bg-gradient-to-tl from-zinc-800 to-zinc-700 leading-pro tracking-tight-rem bg-150 bg-x-25"
+            >
+              Modify
             </button>
           </div>
         </div>
@@ -90,6 +144,7 @@
 export default {
   props: {
     showUserPopup: Boolean,
+    create: Boolean,
   },
   methods: {
     closeUserPopup() {
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -136,148 +136,10 @@
 
       <div class="mt-8 space-y-4 flex" style="flex-direction: column">
         <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
-        >
-          <img
-            class="object-cover w-8 h-8 rounded-full"
-            src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=faceare&facepad=3&w=688&h=688&q=100"
-            alt=""
-          />
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              Mia John
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              11.2 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
-        >
-          <img
-            class="object-cover w-8 h-8 rounded-full"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&h=880&q=80"
-            alt=""
-          />
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              arthur melo
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              1.2 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 bg-gray-100 dark:bg-gray-800 gap-x-2 focus:outline-none"
-        >
-          <div class="relative">
-            <img
-              class="object-cover w-8 h-8 rounded-full"
-              src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&h=764&q=100"
-              alt=""
-            />
-            <span
-              class="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"
-            ></span>
-          </div>
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              Jane Doe
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              15.6 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
-        >
-          <img
-            class="object-cover w-8 h-8 rounded-full"
-            src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&h=764&q=80"
-            alt=""
-          />
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              Amelia. Anderson
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              32.9 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none"
-        >
-          <img
-            class="object-cover w-8 h-8 rounded-full"
-            src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&h=687&q=80"
-            alt=""
-          />
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              Joseph Gonzalez
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              100.2 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
-          class="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800 gap-x-2 focus:outline-none"
-        >
-          <div class="relative">
-            <img
-              class="object-cover w-8 h-8 rounded-full"
-              src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&h=1470&q=80"
-              alt=""
-            />
-            <span
-              class="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"
-            ></span>
-          </div>
-
-          <div class="text-left rtl:text-right">
-            <h1
-              class="text-sm font-medium text-gray-700 capitalize dark:text-white"
-            >
-              Olivia Wathan
-            </h1>
-
-            <p class="text-xs text-gray-500 dark:text-gray-400">
-              8.6 Followers
-            </p>
-          </div>
-        </button>
-
-        <button
+          @click="
+            Userpopup = true;
+            create = false;
+          "
           class="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800 gap-x-2 focus:outline-none"
         >
           <div class="relative">
@@ -304,14 +166,21 @@
           </div>
         </button>
         <button
-          @click="Userpopup = true"
+          @click="
+            Userpopup = true;
+            create = true;
+          "
           class="mx-4 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 items-start justify-center px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded"
         >
           <p class="text-sm font-medium leading-none text-white">Add User</p>
         </button>
       </div>
     </div>
-    <create-user :show-user-popup="Userpopup" @close="Userpopup = false" />
+    <create-user
+      :show-user-popup="Userpopup"
+      :create="create"
+      @close="Userpopup = false"
+    />
   </aside>
 </template>
 
@@ -327,6 +196,7 @@ export default {
     return {
       users: false,
       Userpopup: false,
+      create: false,
     };
   },
 };
