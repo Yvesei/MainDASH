7d3bbf6975fde09af6c863a8f35c248ab816749f
Yvesei <musheck999@gmail.com>
2023-08-17 19:26:32 +0100

    added date picker

--- client/src/components/AddTaskPopup.vue
+++ client/src/components/AddTaskPopup.vue
@@ -26,12 +26,32 @@
                 for="First Name"
                 >Date</label
               >
-              <input
-                type="text"
-                name="First Name"
-                placeholder="eg. Michael"
-                class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
-              />
+
+              <div class="relative">
+                <div
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+                >
+                  <svg
+                    class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  datepicker
+                  type="text"
+                  class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 border border-gray-300 text-gray-900 text-sm leading-5.6 ease block w-full rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Select date"
+                />
+                <!--     class="                                                                      leading-5.6 ease block w-full  appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
+ -->
+              </div>
             </div>
             <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0 sm:w-6/12">
               <label
@@ -265,39 +285,35 @@
                 for="First Name"
                 >Fin de maintenance</label
               >
-              <div class="flex justify-around">
-                <div
-                  class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700"
-                >
-                  <input
-                    id="bordered-checkbox-1"
-                    type="checkbox"
-                    value=""
-                    name="bordered-checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
+              <div>
+                <div class="grid sm:grid-cols-2 gap-2">
                   <label
-                    for="bordered-checkbox-1"
-                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >Favourable</label
+                    for="fav"
+                    class="flex p-3 block w-full bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
                   >
-                </div>
-                <div
-                  class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700"
-                >
-                  <input
-                    checked
-                    id="bordered-checkbox-2"
-                    type="checkbox"
-                    value=""
-                    name="bordered-checkbox"
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
+                    <input
+                      type="checkbox"
+                      class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                      id="fav"
+                    />
+                    <span class="text-sm text-gray-500 ml-3 dark:text-gray-400"
+                      >Favourable</span
+                    >
+                  </label>
+
                   <label
-                    for="bordered-checkbox-2"
-                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >Defavourable</label
+                    for="defa"
+                    class="flex p-3 block w-full bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
                   >
+                    <input
+                      type="checkbox"
+                      class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                      id="defa"
+                    />
+                    <span class="text-sm text-gray-500 ml-3 dark:text-gray-400"
+                      >Defavourable</span
+                    >
+                  </label>
                 </div>
               </div>
             </div>
@@ -317,6 +333,62 @@
               ></textarea>
             </div>
           </div>
+
+          <div class="flex flex-wrap mt-4 -mx-3">
+            <div class="w-full max-w-full px-3 flex-0">
+              <label
+                class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
+                for="First Name"
+                >Maintenance a suivre</label
+              >
+              <div>
+                <div class="grid sm:grid-cols-2 gap-2">
+                  <label
+                    @click="maintenanceAutre = false"
+                    for="a-suivre"
+                    class="flex p-3 block w-full bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+                  >
+                    <input
+                      type="checkbox"
+                      class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                      id="a-suivre"
+                    />
+                    <span class="text-sm text-gray-500 ml-3 dark:text-gray-400"
+                      >Manque des pièces</span
+                    >
+                  </label>
+
+                  <label
+                    @click="maintenanceAutre = true"
+                    for="Autre"
+                    class="flex p-3 block w-full bg-white border border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
+                  >
+                    <input
+                      type="checkbox"
+                      class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 pointer-events-none focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                      id="Autre"
+                    />
+                    <span class="text-sm text-gray-500 ml-3 dark:text-gray-400"
+                      >Autre</span
+                    >
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- when autre is check, show this, need a new function -->
+          <div v-if="maintenanceAutre" class="flex flex-wrap mt-4 -mx-3">
+            <div class="w-full max-w-full px-3 flex-0">
+              <textarea
+                name="Bio"
+                rows="5"
+                placeholder="Say a few words about who you are or what you're working on."
+                class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 min-h-unset text-sm leading-5.6 ease block h-auto w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
+              ></textarea>
+            </div>
+          </div>
+
+          <!--  Buttons  -->
           <div class="flex mt-6">
             <button
               @click="currentDivIndex = (currentDivIndex - 1) % 3"
@@ -363,6 +435,7 @@ export default {
     showPopup: Boolean,
     create: Boolean,
   },
+
   methods: {
     closePopup() {
       this.$emit("close");
@@ -374,6 +447,7 @@ export default {
   data() {
     return {
       currentDivIndex: 0, // Start with the first div
+      maintenanceAutre: false,
     };
   },
 };
