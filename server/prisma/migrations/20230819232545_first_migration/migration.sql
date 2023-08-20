a92282813b4da5f0d61d69a372c1ae4cb9121c49
Yvesei <musheck999@gmail.com>
2023-08-20 01:14:08 +0100

    @unique name instead of number for clients

---
+++ server/prisma/migrations/20230819232545_first_migration/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Client` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX `Client_number_key` ON `Client`;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Client_name_key` ON `Client`(`name`);
