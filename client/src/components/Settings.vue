dc91baf5eb266429a9422e85973f115c11d305af
Yvesei <musheck999@gmail.com>
2023-08-23 20:00:43 +0100

    needs JWT

--- client/src/components/Settings.vue
+++ client/src/components/Settings.vue
@@ -1,5 +1,33 @@
 <template>
   <form class="relative w-2/3 flex mt-6">
+    <!-- component -->
+
+    <div
+      class="flex flex-row items-center justify-center bg-grey-lighter round"
+      style="flex-direction: column"
+    >
+      <label
+        class="flex flex-col items-center px-6 py-6 mb-4 mt-8 bg-white text-blue rounded-full shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white"
+      >
+        <svg
+          class="w-8 h-8"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"
+          />
+        </svg>
+
+        <input type="file" class="hidden" />
+      </label>
+      <label
+        class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
+        for="First Name"
+        >Profile</label
+      >
+    </div>
     <div
       active=""
       form="user"
@@ -68,6 +96,7 @@
               >Nom</label
             >
             <input
+              v-model="name"
               type="text"
               name="First Name"
               placeholder="eg. Michael"
@@ -79,13 +108,14 @@
           <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0">
             <label
               class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
-              for="Last Name"
-              >Number</label
+              for="email"
+              >Email</label
             >
             <input
+              v-model="email"
               type="text"
-              name="Last Name"
-              placeholder="eg. Prior"
+              name="email"
+              placeholder="ex: user@tridentsystems.com"
               class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
             />
           </div>
@@ -94,13 +124,13 @@
           <div class="w-full max-w-full px-3 flex-0">
             <label
               class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
-              for="First Name"
-              >Distance</label
+              for="password"
+              >Password</label
             >
             <input
-              type="text"
-              name="First Name"
-              placeholder="eg. Michael"
+              v-model="password"
+              type="password"
+              name="password"
               class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
             />
           </div>
@@ -109,25 +139,28 @@
           <div class="w-full max-w-full px-3 mt-4 flex-0 sm:mt-0">
             <label
               class="mb-2 ml-1 text-xs font-bold text-slate-700 dark:text-white/80"
-              for="Last Name"
-              >Password</label
+              for="role"
+              >type</label
             >
             <input
-              type="password"
-              name="Last Name"
-              placeholder="eg. Prior"
+              v-model="role"
+              type="text"
+              name="role"
+              placeholder="USER/ADMIN"
               class="focus:shadow-primary-outline dark:bg-slate-850 dark:placeholder:text-white/80 dark:text-white/80 text-sm leading-5.6 ease block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none"
             />
           </div>
         </div>
-        <div class="flex mt-6">
+        <div class="flex mt-6 justify-between">
           <button
+            @click="editUser()"
             type="button"
             aria-controls="address"
             next-form-btn=""
+            href="javascript:;"
             class="inline-block px-6 py-3 mb-0 ml-auto text-xs font-bold text-right text-white uppercase align-middle transition-all ease-in border-0 rounded-lg shadow-md cursor-pointer hover:-translate-y-px active:opacity-85 hover:shadow-xs dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 bg-gradient-to-tl from-zinc-800 to-zinc-700 leading-pro tracking-tight-rem bg-150 bg-x-25"
           >
-            Edit
+            Modify
           </button>
         </div>
       </div>
@@ -136,9 +169,20 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  props: {},
-  methods: {},
+  methods: {
+    editUser() {
+      const response = axios.patch("users/", {
+        id: this.id,
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        role: this.role,
+      });
+    },
+  },
   data() {
     return {};
   },
