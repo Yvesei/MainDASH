46316afebea744d276d7c500f6d7494c88748083
Yvesei <musheck999@gmail.com>
2023-08-24 19:14:46 +0100

    image upload

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,30 @@ var router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+const fileUpload = require("express-fileupload");
+
+// middle ware
+router.use(express.static("public")); //to access the files in public folder
+router.use(fileUpload());
+
+router.post("/upload", (req, res) => {
+  if (!req.files) {
+    return res.status(500).send({ msg: "file is not found" });
+  }
+  // accessing the file
+  const myFile = req.files.file;
+
+  //  mv() method places the file inside public directory
+  myFile.mv(`${__dirname}/../public/uploads/${myFile.name}`, function (err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send({ msg: "Error occured" });
+    }
+    // returing the response with file path and name
+    return res.send({ name: myFile.name, path: `/${myFile.name}` });
+  });
+});
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   const { take, skip } = req.query; // Extract take and skip from query parameters
