255cef44d9c58a27ee55bd3c2f31e83c8f72f1a7
Yvesei <musheck999@gmail.com>
2023-08-24 20:34:10 +0100

    getting image endpoint

--- server/app.js
+++ server/app.js
@@ -20,12 +20,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/tasks", tasksRouter);
 app.use("/login", loginRouter);
 app.use("/clients", clientsRouter);
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 module.exports = app;
